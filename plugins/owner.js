4a352ad4d796096c22f7ac658cafe9c5a16b1e80
HamzabAmirni1 <144018908+HamzabAmirni1@users.noreply.github.com>
2024-07-30 01:38:55 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 معلومات عن صاحب البوت 」*
 
-*Number :*\nwa.me/212717457920
-*instagram:*\ninstagram.com/noureddine_ouafy
+*Number :*\nwa.me/212624855939
+*instagram:*\nhttps://instagram.com/hamza___boyka_
 
-*youtube:*\nyoutube.com/@noureddineouafy2
+*whastsapp :*\nhttps://whatsapp.com/channel/0029Va5mXGNLdQeXkib1M82X
 
-*facebook page:*\nwww.facebook.com/profile.php?id=100063533185520
+*facebook page:*\nwww.facebook.com/profile.php?id=1000635385520
 
 *script bot :* github.com/noureddineouafy
 
